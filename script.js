a39b947a053808ad778134fa8978c18331fae007
Abhilash107 <abhilash00003108884@gmail.com>
2024-03-25 01:35:43 +0530

    completed home page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let menuIcon = document.querySelector('#menu-logo');
 let sidebar=  document.querySelector('.sidebar');
+let container = document.querySelector('.container');
 
 menuIcon.onclick = function(){
-    sidebar.classList.toggle('small-sidebar')
+    sidebar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container')
 }
